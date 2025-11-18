0d02f3b8c0bee380fc3dfe14dedac039ba6a45d6
hojooo <ghwn5833@gmail.com>
2025-11-18 14:52:12 +0900

    feat: product 도메인 엔티티 생성

--- src/main/java/org/sopt/domain/product/entity/Product.java
+++ src/main/java/org/sopt/domain/product/entity/Product.java
@@ -14,4 +14,27 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String originPrice;
+
+    private String salePrice;
+
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private String sizeRange;
+
+    private type productType;
+
+    @Column(nullable = false)
+    private float starAverage;
+
+    @Column(nullable = false)
+    private int reviewCount;
+
 }
